e8aa3c601c4d333f026c30795125a25f386274b5
Luif3rch <84775312+Luif3rch@users.noreply.github.com>
2022-06-11 02:53:23 -0500

    Update BabySeals_TOKEN.sol

--- babyseals/contratos/BabySeals_TOKEN.sol
+++ babyseals/contratos/BabySeals_TOKEN.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract TestToken { 
+contract BABYSEALS { 
 
     string public name = "BABYSEALS";
     string  public symbol = "BS";
